fd61a736c851de54a4472b4a8bf93f1a0770a0e2
145314001 <jacky.avenio@gmail.com>
2016-06-08 07:01:38 +0700

    Add files via upload

---
+++ src/java/model/Mahasiswa.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author HP
+ */
+public class Mahasiswa {
+    private String nama;
+    private String nim;
+    private int totalSKS = 0;
+    private MataKuliah[] makul;
+
+    public Mahasiswa(String nim) {
+        this.nim = nim;
+    }
+
+    public Mahasiswa() {
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public int getTotalSKS() {
+        for (int i = 0; i < makul.length; i++) {
+            setTotalSKS(totalSKS + makul[i].getJumlahSKS());
+        }
+        return totalSKS;
+    }
+
+    public MataKuliah[] getMakul() {
+        return makul;
+    }
+
+    public void setMakul(MataKuliah[] makul) {
+        this.makul = makul;
+    }
+
+    public void setTotalSKS(int totalSKS) {
+        this.totalSKS = totalSKS;
+    }
+    
+    
+    
+}
---
+++ src/java/model/MataKuliah.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author HP
+ */
+public class MataKuliah {
+    private String nama;
+    private String kode;
+    private String dosen;
+    private int jumlahSKS;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getDosen() {
+        return dosen;
+    }
+
+    public void setDosen(String dosen) {
+        this.dosen = dosen;
+    }
+
+    public int getJumlahSKS() {
+        return jumlahSKS;
+    }
+
+    public void setJumlahSKS(int jumlahSKS) {
+        this.jumlahSKS = jumlahSKS;
+    }
+    
+}
